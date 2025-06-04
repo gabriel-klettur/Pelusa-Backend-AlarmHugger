b58e81f33e87f4ff185ac02e381568c3d5cfb729
Ubuntu <ubuntu@ip-172-31-39-154.eu-north-1.compute.internal>
2025-06-04 19:38:43 +0000

    En produccion se utiliza el puerto 8001, de alguna forma desviavamos del 80 al 8001 aqui en linux

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ import signal
 import sys
 from loguru import logger
 
+
+
+#------------------ AlarmHugger ------------------
+
 def signal_handler(sig, frame):
     logger.info('Shutting down gracefully...')
     sys.exit(0)
@@ -14,6 +18,6 @@ signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
     try:
-        uvicorn.run("app.main:app", host="0.0.0.0", port=80, reload=True, log_level="debug")
+        uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True, log_level="debug")
     except KeyboardInterrupt:
         logger.info('Server stopped by user (Ctrl+C)')
